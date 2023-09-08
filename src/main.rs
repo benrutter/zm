d120b110196595dad3af6b15a8bcbf744a120075
Ben <ben@02819>
2023-09-08 10:16:23 +0100

    dont search hidden files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use dirs::home_dir;
 use std::env;
 use std::fs;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,8 +31,20 @@ fn match_in_dir(to_match: &str, dir: &str) -> Option<String> {
     None
 }
 
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn match_in_dir_recursive(to_match: &str, dir: &str) -> Option<String> {
-    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(dir)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(|e| e.ok())
+    {
         if let Some(entry_str) = entry.path().to_str() {
             if entry_str.ends_with(to_match) && entry.path().is_dir() {
                 return Some(entry_str.to_string());
